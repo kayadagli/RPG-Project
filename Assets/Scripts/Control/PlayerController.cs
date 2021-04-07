c82f3370479e4e21d06efae5b5c0eb47332e2f4f
Kaya Dağlı <kayadagli27@gmail.com>
2021-04-07 16:49:33 +0300

    Basic Combat Completed

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -20,7 +20,11 @@ namespace RPG.Control
             foreach (RaycastHit hit in hits)
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
+                // enemy öldüyse, colliderı başka bir enemye saldırmaya engel olmasın diye
+                if (!GetComponent<Fighter>().CanAttack(target)) 
+                {
+                    continue;
+                }
 
                 if (Input.GetMouseButtonDown(0))
                 {
