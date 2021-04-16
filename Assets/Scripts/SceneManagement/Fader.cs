833c23a4c23beedb2522987644e385ef8b36549f
Kaya Dağlı <kayadagli27@gmail.com>
2021-04-16 19:43:57 +0300

    First scene completed and scene management done (moving from one scene to another)

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            yield return FadeIn(1.5f);
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) 
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
